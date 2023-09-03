e5ee4481cdbc2e37a8a1a8d94c78db4efc7ec89f
eabdyushev <eabdyushev@nobilis.team>
2023-09-04 01:16:22 +0600

    technical commit v3

--- base.py
+++ base.py
@@ -1,19 +1,22 @@
 import datetime
 import json
 import os
+import platform
 import subprocess
-import sys
 import threading
 import time
 import tkinter as tk
 from tkinter import ttk, messagebox
 
-from utility_function import basis_handle_errors, Log
+from utility_function import basis_handle_errors
 
 
 @basis_handle_errors(text='PackageGeneratorApp')
 class PackageGeneratorApp:
     def __init__(self, root):
+        self.result_string = None
+        self.current_datetime = None
+        self.formatted_datetime = None
         self.scrollbar = None
         self.check_buttons_frame_inner = None
         self.canvas = None
@@ -35,13 +38,11 @@ class PackageGeneratorApp:
         self.x_position = (self.screen_width - self.window_width) // 2
         self.y_position = (self.screen_height - self.window_height) // 2
         self.root.geometry(f"{self.window_width}x{self.window_height}+{self.x_position}+{self.y_position}")
-
+        self.ctrl_pressed = False
         self.packages = []
         self.selected_packages = []
 
-        self.current_datetime = datetime.datetime.now()
-        self.formatted_datetime = self.current_datetime.strftime("%d.%m.%Y_%H.%M.%S")
-        self.result_string = f"packages_{self.formatted_datetime}"
+        self.set_default_name_pkg()
 
         self.path_var = tk.StringVar()
         self.path_var.set("")
@@ -66,17 +67,53 @@ class PackageGeneratorApp:
         self.root.config(menu=self.menu_bar)
 
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.file_menu.add_command(label="Выход", command=root.quit)
+        self.file_menu.add_command(label="Открыть папку сохранения [Ctrl+o]", command=self.open_folder)
+        self.root.bind("<Control-o>", self.open_folder_key)
+        self.file_menu.add_command(label="Выход [Ctrl+q]", command=self.quit_program)
+        self.root.bind("<Control-q>", self.quit_program)
         self.menu_bar.add_cascade(label="Меню", menu=self.file_menu)
 
         self.help_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.help_menu.add_command(label="Справка", command=self.show_help)
+        self.help_menu.add_command(label="Справка [Ctrl+h]", command=self.show_help)
+        self.root.bind("<Control-h>", self.show_help_key)
         self.menu_bar.add_cascade(label="Помощь", menu=self.help_menu)
+
+        self.current_key_sequence = []
+
+        self.key_commands = [
+            {"key_sequence": "<Control-Key-1>", "command": self.select_all_checkboxes_key},
+            {"key_sequence": "<Control-Key-2>", "command": self.open_packages_input_key},
+            {"key_sequence": "<Control-Key-3>", "command": self.add_path_for_pkg_key},
+
+            {"key_sequence": "<Control-s>", "command": self.save_settings_key},
+            {"key_sequence": "<Control-r>", "command": self.run_command_key},
+            {"key_sequence": "<Control-b>", "command": self.generate_command_key},
+        ]
+
+        for item in self.key_commands:
+            key_sequence = item["key_sequence"]
+            command = item["command"]
+            self.root.bind(key_sequence, self.execute_command(command))
         # --------------------------------------------------------------------------------------------------------------
         if self.package_name_var.get() == "":
             self.package_name_var.set(f"{self.result_string}")
 
         self.create_widgets()
+
+    @staticmethod
+    def execute_command(command):
+        def wrapped(event):
+            command(event)
+
+        return wrapped
+
+    def set_default_name_pkg(self):
+        self.get_current_date_whit_format()
+        self.result_string = f"packages_{self.formatted_datetime}"
+
+    def get_current_date_whit_format(self):
+        self.current_datetime = datetime.datetime.now()
+        self.formatted_datetime = self.current_datetime.strftime("%d.%m.%Y_%H.%M.%S")
 
     def create_widgets(self):
         self.package_name_var.trace_add("write", self.check_text)
@@ -101,7 +138,7 @@ class PackageGeneratorApp:
         button_open_packages.place(width=110, height=27, x=560.5, y=23)
         # --------------------------------------------------------------------------------------------------------------
         button_add_path = ttk.Button(frame_1, text="Добавить путь", command=self.add_path_for_pkg,
-                                         style="My.TButton")
+                                     style="My.TButton")
         button_add_path.place(width=100, height=27, x=685, y=23)
         # --------------------------------------------------------------------------------------------------------------
         label_selected_path = ttk.Label(frame_1, text="Путь до папки Pkg")
@@ -138,7 +175,7 @@ class PackageGeneratorApp:
         self.progressbar = ttk.Progressbar(self.root, mode='determinate', length=235)
         self.progressbar.place(x=268, y=362)
         # --------------------------------------------------------------------------------------------------------------
-        self.time_label = ttk.Label(self.root, text="Время выполнения: 0.0 мин.")
+        self.time_label = ttk.Label(self.root, text="Сборка началась: 0.0 мин.")
         self.time_label.pack_forget()
 
     def check_text(self, *args):
@@ -149,17 +186,27 @@ class PackageGeneratorApp:
         if entered_text.endswith("_clear"):
             self.save_to_settings_one_attribute("package_name_var", "")
             self.package_name_var.set(f"{self.result_string}")
+        if entered_text.endswith("_update"):
+            self.set_default_name_pkg()
+            self.package_name_var.set(f"{self.result_string}")
         else:
             pass
 
-    @staticmethod
-    def restart_program():
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
+    def quit_program(self, event):
+        self.root.quit()
+
+    def select_all_checkboxes_key(self, event):
+        self.select_all_checkboxes()
 
     def select_all_checkboxes(self):
         for package, var, cb in self.check_buttons:
-            var.set(1)
+            var.set(1 - var.get())
+
+    def is_at_least_one_checkbox_selected(self):
+        for package, var, cb in self.check_buttons:
+            if var.get() == 1:
+                return True
+        return False
 
     def paste(self, event):
         self.input_text.event_generate("<<Paste>>")
@@ -169,6 +216,24 @@ class PackageGeneratorApp:
         self.input_text.mark_set(tk.INSERT, "1.0")
         self.input_text.see(tk.INSERT)
         return "break"
+
+    def open_folder_key(self, event):
+        self.open_path_in_explorer()
+
+    def open_folder(self):
+        self.open_path_in_explorer()
+
+    def open_path_in_explorer(self):
+        path = self.path_var.get()
+        system = platform.system()
+        if system == "Windows":
+            os.system(f"start explorer {path}")
+        elif system == "Darwin":  # macOS
+            os.system(f"open {path}")
+        elif system == "Linux":
+            os.system(f"xdg-open {path}")
+        else:
+            print("Неизвестная операционная система, не удалось открыть папку.")
 
     def on_path_selected(self, event):
         self.path_to_pkg_var.set(self.path_to_pkg_combobox.get())
@@ -203,22 +268,48 @@ class PackageGeneratorApp:
 
         self.check_buttons_frame_inner.update_idletasks()
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-        self.canvas.bind_all("<MouseWheel>", self.on_mousewheel)
+        self.canvas.bind("<MouseWheel>", self.on_mousewheel)
 
     def on_mousewheel(self, event):
         self.canvas.yview_scroll(-1 * (event.delta // 120), "units")
 
-    def show_help(self):
-        help_text = "1) Что бы сохранить название добавьте в конце строки [_save | _clear]."
+    def show_help_key(self, event):
+        self.show_help()
 
-        self.create_window("Справка", help_text, show_save_button=False, width=600, height=200)
+    def show_help(self):
+        help_text = (
+            "Горячие клавиши:\n\n"
+            "[Ctrl+1] - [Выбрать\\Отменить выбор] все(х) пакеты(ов)\n"
+            "[Ctrl+2] - Добавить пакеты\n"
+            "[Ctrl+3] - Добавить путь\n\n"
+
+            "[Ctrl+s] - Сохранить настройки\n"
+            "[Ctrl+r] - Запустить сборку\n"
+            "[Ctrl+b] - Собрать команду\n\n"
+
+            "[Ctrl+o] - Открыть папку сохранения\n"
+            "[Ctrl+h] - Справка\n"
+            "[Ctrl+q] - Выход\n\n"
+            "Команды которые можно выполнять в поле ввода [Наименование пакета]:\n"
+            "(? вводить команды нужно в конце текста [pkg_v1_save])\n"
+            "[_save | _clear | _update]\n\n"
+            "[_save] - сохраняет введённое наименование пакета в файл настроек.\nПри следующим открытии приложения в поле [Наименование пакета] будет выведено сохранённое наименование пакета\n\n"
+            "[_clear] - чистит введённое наименование пакета в файл настроек.\nПри следующим открытии приложения в поле [Наименование пакета] будет выведено дефолтное наименование пакета [packages_<Текущая дата и время в формате [%d.%m.%Y_%H.%M.%S]>]\n\n"
+            "[_update] - обновляет поле [Наименование пакета] дефолтным наименованием [packages_<Текущая дата и время в формате [%d.%m.%Y_%H.%M.%S]>]")
+
+        self.create_window("Справка", help_text, show_save_button=False, width=600, height=400, editable=False)
 
     @basis_handle_errors(text='create_window')
-    def create_window(self, title, initial_text, on_save=None, show_save_button=True, width=600, height=300):
-        window = tk.Toplevel(self.root)
+    def create_window(self, title, initial_text, on_save=None, show_save_button=True, width=600, height=300,
+                      is_fix_size=False, editable=True):
+        window = tk.Toplevel(self.root, width=width, height=height)
+
+        if is_fix_size:
+            window.maxsize(width=width, height=height)
+            window.minsize(width=width, height=height)
+
         window.title(title)
         icon_path = os.path.join('icons', 'icon.ico')
-        Log.info(icon_path, "icon_path")
         window.iconbitmap(icon_path)
 
         window_width = width
@@ -231,15 +322,18 @@ class PackageGeneratorApp:
         x = parent_x + (parent_width - window_width) // 2
         y = parent_y + (parent_height - window_height) // 2
 
-        # Устанавливаем позицию нового окна
         window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-        input_text = tk.Text(window)
+        input_text = tk.Text(window, width=width, height=height)
 
         input_text.pack()
         input_text.insert(tk.END, initial_text)
-        input_text.bind("<Control-v>", self.paste)
-        input_text.bind("<Control-a>", self.select_all)
+
+        if not editable:
+            input_text.config(state="disabled")
+        else:
+            input_text.bind("<Control-v>", self.paste)
+            input_text.bind("<Control-a>", self.select_all)
 
         if on_save and show_save_button:
             def save_and_close():
@@ -250,6 +344,9 @@ class PackageGeneratorApp:
             (ttk.Button(window, text="Сохранить", command=save_and_close)
              .place(width=115, height=27, x=465, y=258))
 
+    def open_packages_input_key(self, event):
+        self.open_packages_input()
+
     def open_packages_input(self):
         initial_text = ", ".join(self.packages)
 
@@ -259,12 +356,15 @@ class PackageGeneratorApp:
             self.create_check_buttons()
             self.save_settings()
 
-        self.create_window("Введите пакеты", initial_text, on_save)
+        self.create_window("Введите пакеты", initial_text, on_save, is_fix_size=True)
 
     def update_path_combobox(self):
         result_string = self.delete_duplicate_for_str()
         values = result_string.split(",")
         self.path_to_pkg_combobox['values'] = values
+
+    def add_path_for_pkg_key(self, event):
+        self.add_path_for_pkg()
 
     def add_path_for_pkg(self):
         initial_text = self.path_for_pkg_var.get()
@@ -274,7 +374,10 @@ class PackageGeneratorApp:
             self.update_path_combobox()
             self.save_settings()
 
-        self.create_window("Введите путь для пакета", initial_text, on_save)
+        self.create_window("Введите путь для пакета", initial_text, on_save, is_fix_size=True)
+
+    def generate_command_key(self, event):
+        self.generate_command()
 
     def generate_command(self):
         selected_packages = [package for package, var, cb in self.check_buttons if var.get() == 1]
@@ -304,6 +407,9 @@ class PackageGeneratorApp:
                 pass
         else:
             self.package_name_var.set("")
+
+    def save_settings_key(self, event):
+        self.save_settings()
 
     def save_settings(self):
         self.packages = list(set(self.packages))
@@ -335,19 +441,58 @@ class PackageGeneratorApp:
         result_string = ",".join(unique_lines)
         return result_string
 
-    def run_command(self):
-        self.generate_command()
-        self.start_time = time.time()
+    def run_command_key(self, event):
+        self.run_command()
 
+    def run_command(self):
+        if not self.is_at_least_one_checkbox_selected():
+            messagebox.showinfo("Сборка", "Для запуска команды нужно выбрать хотя бы один пакет.")
+            return
+
+        self.generate_command()
+        current_directory = os.getcwd()
+
+        def handle_command_execution(result, start_time):
+            self.progressbar.stop()
+            self.progressbar.config(mode='determinate')
+            self.stop_timer_event.set()
+            self.time_label.pack_forget()
+
+            end_time = time.time()
+            execution_time = end_time - start_time
+            minutes = int(execution_time // 60)
+            fraction_minutes = execution_time % 60
+
+            output_message = result.stdout + "\n\n" + (f"Сборка пакета [{self.package_name_var}] завершилась успешно!"
+                                                       f"\nВремя выполнения: {minutes:.0f}.{fraction_minutes:.0f} мин.")
+            if result.stderr:
+                output_message += "Стандартный вывод ошибок:\n" + result.stderr
+            messagebox.showinfo("Результат выполнения команды", output_message)
+
+        def execute_command_internal():
+            try:
+                os.chdir(self.path_to_pkg_var.get())
+
+                result = subprocess.run(['powershell.exe', '-Command', self.generated_command_var.get()], shell=True,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE, text=True)
+
+                handle_command_execution(result, start_time)
+
+            except Exception as ex:
+                self.handle_command_error(ex, start_time)
+
+            finally:
+                os.chdir(current_directory)  # Возвращаемся в исходную директорию после выполнения команды
+
+        # Получаем путь и команду
         selected_path = self.path_to_pkg_var.get()
         command = self.generated_command_var.get()
-
         if selected_path and command:
             confirmation = messagebox.askyesno("Подтверждение",
                                                f"Вы уверены, что хотите выполнить команду в каталоге:\n{selected_path}\n\nКоманда:\n{command}")
             if confirmation:
                 try:
-                    current_directory = os.getcwd()
                     os.chdir(selected_path)
                     self.progressbar.config(mode='indeterminate')
                     self.progressbar.start()
@@ -357,45 +502,22 @@ class PackageGeneratorApp:
                     update_time_thread = threading.Thread(target=self.update_execution_time)
                     update_time_thread.start()
 
-                    def execute_command():
-                        try:
-                            result = subprocess.run(['powershell.exe', '-Command', command], shell=True,
-                                                    stdout=subprocess.PIPE,
-                                                    stderr=subprocess.PIPE, text=True)
-                            self.progressbar.stop()
-                            self.progressbar.config(mode='determinate')
-                            self.stop_timer_event.set()
-                            self.time_label.pack_forget()
-
-                            end_time = time.time()
-                            execution_time = end_time - start_time
-                            minutes = int(execution_time // 60)
-                            fraction_minutes = execution_time % 60
-
-                            output_message = result.stdout + "\n\n" + f"Время выполнения: {minutes:.0f}.{fraction_minutes:.0f} мин."
-                            if result.stderr:
-                                output_message += "Стандартный вывод ошибок:\n" + result.stderr
-                            messagebox.showinfo("Результат выполнения команды", output_message)
-                        except Exception as e:
-                            self.progressbar.stop()
-                            self.progressbar.config(mode='determinate')
-                            self.stop_timer_event.set()
-                            self.time_label.pack_forget()
-                            print("Ошибка выполнения команды:", str(e))
-                        finally:
-                            end_time = time.time()
-                            execution_time = end_time - start_time
-                            print(execution_time)
-                            os.chdir(current_directory)
-
-                    command_thread = threading.Thread(target=execute_command)
+                    command_thread = threading.Thread(target=execute_command_internal)
                     command_thread.start()
+
                 except Exception as e:
-                    self.progressbar.stop()
-                    self.progressbar.config(mode='determinate')
-                    self.stop_timer_event.set()
-                    self.time_label.pack_forget()
-                    print("Ошибка выполнения команды:", str(e))
+                    self.handle_command_error(e, start_time)
+
+    # Метод для обработки ошибок выполнения команды
+    def handle_command_error(self, error, start_time):
+        self.progressbar.stop()
+        self.progressbar.config(mode='determinate')
+        self.stop_timer_event.set()
+        self.time_label.pack_forget()
+        print("Ошибка выполнения команды:", str(error))
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(execution_time)
 
     def update_execution_time(self):
         while not self.stop_timer_event.is_set():
